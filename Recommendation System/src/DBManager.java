14afb169b52bfcb7115388bd88f2276627358c96
Andrea <andrea.alessandro90@gmail.com>
2015-09-02 18:16:28 +0200

    Connessione a db

--- Recommendation System/src/DBManager.java
+++ Recommendation System/src/DBManager.java
@@ -1,11 +1,27 @@
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 
 public class DBManager
 {
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		//connection with Mongodb Server
+		MongoClient client = new MongoClient("localhost", 27017);
+		
+		MongoDatabase db = client.getDatabase("user_data");
+		
+		MongoCollection<Document> collection = db.getCollection("log");
+		System.out.println("Count: " + collection.count());
+		
+		Document doc = collection.find().first();
+		System.out.println(doc.toJson());
+		
+		client.close();
 	}
 
 }
