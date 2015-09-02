fa16e1ea19488648c513dd81494c9e6f04521bdb
Andrea <andrea.alessandro90@gmail.com>
2015-09-02 19:49:07 +0200

    DBManager migliorato

--- Recommendation System/src/DBManager.java
+++ Recommendation System/src/DBManager.java
@@ -4,24 +4,61 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+public class DBManager {
 
-public class DBManager
-{
+	private static final String SERVER_ADDRESS = "localhost";
+	private static final int PORT = 27017;
+	private static final String DATABASE_NAME = "user_data";
+	private static MongoClient client = null;
 
-	public static void main(String[] args)
-	{
-		//connection with Mongodb Server
-		MongoClient client = new MongoClient("localhost", 27017);
+	private MongoDatabase database;
+	
+	public DBManager() {
+
+	}
+
+	public void startConnection(){
+		if(client == null){
+			client = new MongoClient(SERVER_ADDRESS, PORT);
+		}
+		else{
+			System.out.println("Connection alrady started");
+		}
+	}
+
+	public void closeConnection(){
+		client.close();
+	}
+	
+	public void setDatabase(String databaseName){
 		
-		MongoDatabase db = client.getDatabase("user_data");
+		database = client.getDatabase(databaseName);
+	}
+	
+	public MongoDatabase getDatabase(){
+		return database;
+	}
+
+	public MongoCollection<Document> getCollectionByName(String collectionName){
+		MongoCollection<Document> collection = database.getCollection(collectionName);
+		return collection;
+	}
+	
+	public static void main(String[] args) {
+
+		// connection with Mongodb Server
+		DBManager manager = new DBManager();
+		manager.startConnection();
 		
-		MongoCollection<Document> collection = db.getCollection("log");
+		manager.setDatabase(DATABASE_NAME);
+
+		MongoCollection<Document> collection = manager.getCollectionByName("log");
 		System.out.println("Count: " + collection.count());
-		
+
 		Document doc = collection.find().first();
 		System.out.println(doc.toJson());
-		
-		client.close();
+
+		manager.closeConnection();
 	}
 
 }
